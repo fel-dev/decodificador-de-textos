177f31bc6aec4200f8389d45cc5fec2be03907e0
Felipe <felipecarneiro@duck.com>
2022-05-05 00:43:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-//Javascript Replace(): conheça esse método e aprenda como usar! https://blog.betrybe.com/javascript/javascript-replace/
-
 const inputTexto = document.querySelector(".input-texto")
 const mensagem = document.querySelector(".mensagem")
 
+const h6 = document.querySelector("#h6")
+const p = document.querySelector("#p")
+
+const matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+
 function btnEncriptar() {
     const textoEncriptado = encriptar(inputTexto.value)
-    mensagem.value = textoEncriptado
-    mensagem.style.backgroundImage="none"
+    if (inputTexto.value == '') {
+        inputTexto.placeholder = 'Este campo é obrigatório'
+        if (mensagem.placeholder.length != 0) {
+            resetMensagem()
+        }
+    } else {
+        inputTexto.placeholder = 'Digite seu texto'
+        mensagem.value = textoEncriptado
+        mensagem.style.backgroundImage="none"
+    }
 }
 
 function encriptar(stringEncriptada) {
-    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase()
 
     for(let i = 0; i < matrizCodigo.length; i++) {
@@ -23,13 +33,22 @@ function encriptar(stringEncriptada) {
 
 function btnDesencriptar () {
     const textoEncriptado = desencriptar(inputTexto.value)
-    mensagem.value = textoEncriptado
-    mensagem.style.backgroundImage="none"
+    if (inputTexto.value == '') {
+        inputTexto.placeholder = 'Este campo é obrigatório'
+        if (mensagem.placeholder.length != 0) {
+            resetMensagem()
+        }
+    } else {
+        inputTexto.value = ''
+        inputTexto.placeholder = 'Digite seu texto.'        
+        mensagem.value = textoEncriptado
+        mensagem.style.backgroundImage="none"
+        // window.alert("Sucesso!!!")
+    }
 }
 
 
 function desencriptar(stringEncriptada) {
-    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase()
 
     for(let i = 0; i < matrizCodigo.length; i++) {
@@ -43,11 +62,28 @@ function desencriptar(stringEncriptada) {
 function btnCopiar() {
     if (mensagem.value == '') {
         mensagem.placeholder = 'Nenhuma mensagem encontrada'
-    } else {
+        inputTexto.placeholder = 'Informe uma frase'
+        mensagem.style.backgroundImage="none"
+    }  else {
         navigator.clipboard.writeText(mensagem.value);
         mensagem.value = ''
         mensagem.placeholder = 'Mensagem copiada com sucesso!'
+        inputTexto.placeholder = 'Digite seu texto'
+        
     }
+}
+
+function btnColar() {    
+        navigator.clipboard.writeText(inputTexto.value);
+        mensagem.value = ''
+        mensagem.placeholder = 'Mensagem copiada com sucesso!'    
+}
+
+function resetMensagem () {
+    mensagem.style.backgroundImage='url("/imagens/resultado.png")'
+    mensagem.placeholder = ''
+    h6.value = 'título aqui h6'
+    p.value = 'parágrafo da mesnagem aqui'
 }
 
 
@@ -59,26 +95,3 @@ inputTexto.onkeydown = function (e) {
     }
 }
 
-
-
-    //////////////////////////////////////////
-    ///////////// T E S T E S ////////////////
-    //////////////////////////////////////////
-
-bt_teste_encod = document.querySelector("#bt_teste_encod")
-bt_teste_decod = document.querySelector("#bt_teste_decod")
-
-bt_teste_encod.addEventListener('click', teste_e)
-bt_teste_decod.addEventListener('click', teste_d)
-
-function teste_e () {
-    teste = "a e i o u"
-    txt.value = teste
-    res.value = encode(teste)
-}
-
-function teste_d () {
-    teste = "aimes enter imes ober ufat"
-    txt.value = teste
-    res.value = decode(teste)
-}
